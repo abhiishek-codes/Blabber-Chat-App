eaccfdea75e2bd91ff857a38d9bb5ce8fa49031b
abhiishek-codes <156610952+abhiishek-codes@users.noreply.github.com>
2024-05-28 17:19:41 +0530

    fix cors

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://blabber-pwhtqbeth-abhisheks-projects-e2710dbc.vercel.app",
+    origin: "https://blabber-phi.vercel.app",
   })
 );
 app.get("/", (req, res) => {
